3084f597221198524aad7c134bd6aea1a592038c
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-07 17:47:43 +0900

    Export symmetry-pruned proof DAG API

--- adaptive_gain/__init__.py
+++ adaptive_gain/__init__.py
@@ -144,6 +144,17 @@ from .proof_dag import (
     compress_fixed_budget_infeasibility_proof,
     verify_cover_proof_dag,
 )
+from .symmetry_pruned_proof_dag import (
+    SymmetricBranchMember,
+    SymmetricBranchOrbit,
+    SymmetryPrunedAdaptiveGainAudit,
+    SymmetryPrunedProofDagCertificate,
+    SymmetryPrunedProofDagLimitError,
+    SymmetryPrunedProofDagNode,
+    build_symmetry_pruned_proof_dag,
+    selected_policy_symmetry_pruned_gain_audit,
+    verify_symmetry_pruned_proof_dag,
+)
 
 __all__ = [
     "ResidualAutomorphismGroupReceipt", "residual_automorphism_group",
@@ -202,4 +213,9 @@ __all__ = [
     "selected_policy_pair_packing_gain_certificate",
     "CoverProofDagBranch", "CoverProofDagCertificate", "CoverProofDagNode",
     "compress_fixed_budget_infeasibility_proof", "verify_cover_proof_dag",
+    "SymmetricBranchMember", "SymmetricBranchOrbit",
+    "SymmetryPrunedAdaptiveGainAudit", "SymmetryPrunedProofDagCertificate",
+    "SymmetryPrunedProofDagLimitError", "SymmetryPrunedProofDagNode",
+    "build_symmetry_pruned_proof_dag", "selected_policy_symmetry_pruned_gain_audit",
+    "verify_symmetry_pruned_proof_dag",
 ]
